396d24354940ec95bc93610ffebf9edb67aebb3c
Jerry Shanahan <jerry.shanahan@gmail.com>
2017-02-15 15:59:05 -0500

    deleteMultipleOccurrences

--- deleteMultipleOccurrences.js
+++ deleteMultipleOccurrences.js
@@ -14,7 +14,7 @@ For example if N = 2, and the input is [1,2,3,1,2,1,2,3],
 you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, 
 and then take 3, which leads to [1,2,3,1,2,3].
 */
-
+ 
 function deleteNth(arr,x){
   let result = [];
   let obj = arr.reduce((acc, num) => {
